ae0f991c335c00440f43b2eb0d89d4973cfde211
iamnotpi <ductrung27.dt@gmail.com>
2025-05-20 10:01:15 +0700

    SFT Qwen3 instead.

--- sft_trainer.py
+++ sft_trainer.py
@@ -3,7 +3,7 @@ from trl import SFTConfig, SFTTrainer
 from datasets import load_dataset
 import torch
 
-model_name = "Qwen3-1.7B-DAPO"
+model_name = "Qwen/Qwen3-1.7B"
 
 # Load model and tokenizer
 model = AutoModelForCausalLM.from_pretrained(
@@ -14,36 +14,39 @@ model = AutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 ds = load_dataset("justus27/light-r1-stage-2-sft", split='train')
-system_prompt = "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer, and put your final answer within \\boxed{{}}. The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>."
-def format_R1(example):
-    prompt = []
-    prompt.append({"role": "system", "content": system_prompt})
-    prompt.extend(example['messages'])
-    return {"messages": prompt}
+# system_prompt = "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer, and put your final answer within \\boxed{{}}. The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>."
+# def format_R1(example):
+#     prompt = []
+#     prompt.append({"role": "system", "content": system_prompt})
+#     prompt.extend(example['messages'])
+#     return {"messages": prompt}
 
-ds = ds.map(format_R1)
+# ds = ds.map(format_R1)
 
 training_args = SFTConfig(
-    output_dir="Qwen3-1.7B-DAPO-SFT-stage-2",
+    output_dir="Qwen3-1.7B-SFT",
     per_device_train_batch_size=8,
     gradient_accumulation_steps=4,
-    max_seq_length=32768,
+    max_seq_length=20480,
     learning_rate=1e-5,
     num_train_epochs=8,
     gradient_checkpointing=True,
     use_liger_kernel=True,
+    lr_scheduler_type='cosine_with_min_lr',
+    lr_scheduler_kwargs={'min_lr_rate': 0.1},
+    warmup_ratio=0.1,
     eos_token='<|im_end|>',
     bf16=True,
     tf32=True,
-    torch_compile=True,
+    # torch_compile=True,
     logging_steps=1,
     logging_dir="./logs",  
     logging_strategy="steps",  
-    logging_first_step=True,  # Log the first training step
-    log_level="info",  # Logging level (info, warning, error, etc)
+    logging_first_step=True,  
+    log_level="info",  
     save_strategy="epoch", 
     push_to_hub=True,
-    hub_model_id="Pi-1905/Qwen3-1.7B-DAPO-SFT-stage-2",
+    hub_model_id="Pi-1905/Qwen3-1.7B-SFT",
     hub_strategy="all_checkpoints",
     report_to="wandb",
 )
